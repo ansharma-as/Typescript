d6ffc376f3c71fae55da873c515ddb9bbf97877f
ansharma-as <anshsharma712.as@gmail.com>
2025-01-30 21:07:42 +0530

    added TypeScript#1

---
+++ a.js
@@ -0,0 +1,11 @@
+"use strict";
+const x = 1;
+console.log(x);
+function greet(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+greet("Ansh");
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(1, 2));
---
+++ a.ts
@@ -0,0 +1,15 @@
+const x: number =1;
+console.log(x);
+
+
+function greet(firstName: string){
+    console.log(`Hello, ${firstName}!`);
+}
+
+greet("Ansh");
+
+
+function sum(a: number , b: number){
+    return a + b;
+}
+console.log(sum(1,2));
